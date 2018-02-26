6242e53a2e8e092298d047a897501644db7ba43a
Your Name <you@example.com>
2018-02-26 09:22:29 +0000

    Use mapcat

--- python/gec/train.py
+++ python/gec/train.py
@@ -183,19 +183,13 @@ def lt(x, y):
     return x > y
 
 
-def concat(*colls):
-    for coll in colls:
-        for element in coll:
-            yield element
-
-
 def make_get_variables(m):
     def get_variables(k):
         return get_variables_(
             merge(m,
                   {"k": k,
                    "raw_batches":
-                       apply(concat, map(compose(
+                       mapcat(compose(
                            partial(partition, if_(m["split"] == "training",
                                                   m["batch_size"],
                                                   1)),
@@ -207,7 +201,7 @@ def make_get_variables(m):
                                                flip(get),
                                                "bpe"))),
                            get_raw_data),
-                           glob.glob(get_glob(m))))}))
+                           glob.glob(get_glob(m)))}))
     return get_variables
 
 
